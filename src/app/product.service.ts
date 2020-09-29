10af383b48c18344580a021a1851a6f1e4cb0494
Saadullah Olawale IMRAN <progrmanial@gmail.com>
2020-09-29 14:37:52 +0100

    Work in progress

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -18,7 +18,7 @@ export class ProductService {
   }
 
   getProducts() :Observable<Product[]>{
-    return this._http.get(this._productsUrl).map(response => response.json() as <Product[]> );
+    return this._http.get(this._productsUrl).map(response => (Product[]) response.json() );
   }
 
 }
