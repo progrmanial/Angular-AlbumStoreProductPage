71f1933bb53e343671843d91c2368431665d64fa
Saadullah Olawale IMRAN <progrmanial@gmail.com>
2020-09-29 15:47:30 +0100

    Work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes:Routes = [{ path: products, components: ProductListComponent }, {path: 'product/:id', component: ProductPageComponent}];
+const appRoutes:Routes = [{ path: 'products', components: ProductListComponent }, {path: 'product/:id', component: ProductPageComponent}];
 
 @NgModule({
   declarations: [
