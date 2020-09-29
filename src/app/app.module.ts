446a88654cef34e92ad45c893011f2a1834e5b49
Saadullah Olawale IMRAN <progrmanial@gmail.com>
2020-09-29 16:22:13 +0100

    Work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes = [
-  { path: 'products', components: ProductListComponent }, {path: 'product/:id', component: ProductPageComponent},
-  {path: '', redirectTo: 'products', pathmatch: 'full'}
+  { path: 'products', components: ProductListComponent },
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
 ];
 
 @NgModule({
